c921511657c8f87ba470503573986846e996e1a1
Justin <klofseattle@hotmail.com>
2015-12-20 10:26:09 -0800

    fixed ver

--- README.md
+++ README.md
@@ -10,3 +10,5 @@ Writer: Justin Byun
 voting works! but notsure how to store the results in array and eventually graph from them
 
 instead, simple chat box function was added to leave comments!-SOOOOO CUUUUUUTE!
+
+ONLY showing the pic that wins the vote!
--- events.js
+++ events.js
@@ -4,10 +4,23 @@
 
 //reset with alert msg
 
- $('#clear').click(function(){
-	alert('Sorry, please try again!');
-	$('#clear').empty;
-});
+//  $('#clear').click(function(){
+// 	alert('Sorry, please try again!');
+// 	$('#clear').empty;
+// });
+
+ $('#leftButton').click(function(){
+ 	$('#rightCat').empty();
+ 	
+ 	
+ })
+
+ $('#rightButton').click(function(){
+ 	$('#leftCat').empty();
+ 	
+ })
+
+
 
 // var statsButton = document.getElementById('results');
 // 	userInput.addEventListener('results',function(event){
@@ -17,39 +30,3 @@
 //catAlbum =[cat];
 
 //});
-///////////Cat Charts/////////////////////////////
-// var data = {
-//     labels: ["cat0", "cat1", "cat2", "cat3", "cat4",'cat5','cat6','cat7','cat8','cat9','cat10','cat11','cat12','cat13','cat14','cat15','cat16','cat17','cat18','cat19','cat20'],
-//     datasets: [
-//          {
-//             label: catAlbum.map(function(cat){
-//                 return cat.place;
-//                 //return shop.place;
-//             }),
-//             label: "Cuttest cats in the World",
-//             fillColor: "rgba(0,63,76,0.75)",
-//             strokeColor: "rgba(220,220,220,1)",
-//             highlightFill: "rgba(220,220,220,1)",
-//             highlightStroke: "rgba(220,220,220,1)",
-
-            
-//             data: catAlbum.map(function(cat){
-//                 return catAlbum(cat);
-//              })
-//         }
-        
-//     ]
-// };
-// var ctx = document.getElementById("myChart").getContext("2d");
-// var options = { 
-//     scaleBeginAtZero : true,
-//     scaleShowHorizontalLines: true,
-//     scaleGridLineColor : "rgba(0,0,0,1)"
-
-// };
-// var myBarChart = new Chart(ctx).Bar(data, options);
-
-
-
-
-
